22d633ac7d1941dbe6522d56e92841618992c9f3
Mike Love <michaelisaiahlove@gmail.com>
2014-01-14 11:25:25 -0500

    shist() removed color and lwd

--- R/shist.R
+++ R/shist.R
@@ -1,30 +1,33 @@
-shist <- function(z, unit, plotHist = TRUE, add = FALSE,
-                  lineCol = "dodgerblue", xlim, ylim) {
-    n <- length(z)
-    main <- paste("Shistogram of", deparse(substitute(z)))
-    d <- density(z)
-    ymax <- max(d$y * n * unit)
-    if (missing(xlim)) {
-        xlim <- c(min(z) - unit, max(z) + unit)
-    }
-    if (missing(ylim)) {
-        ylim <- c(0, 1.1 * ymax)
-    }
-    if (plotHist) {
-        h <- hist(z, breaks = seq(from = min(z) - unit, to = max(z) + unit, 
-            by = unit), plot = FALSE)
-        ylim[2] <- max(ylim[2], 1 * 1 * h$count)
-        plot(h, col = "grey", main = main, xlab = deparse(substitute(z)), ylim = ylim, 
-            xlim = xlim)
-    } else {
-        if (!add) {
-            plot(0, 0, type = "n", xlim = xlim, ylim = ylim, xlab = deparse(substitute(z)), 
-                ylab = "Frequency", main = main)
-        }
-    }
-    lines(d$x, d$y * n * unit, lwd = 3, col = lineCol)
+shist <- function(z, unit = 1, plotHist = FALSE, add = FALSE,
+                  xlim, ylim, ...) {
+  n <- length(z)
+  main <- paste("Shistogram of", deparse(substitute(z)))
+  d <- density(z)
+  ymax <- max(d$y * n * unit)
+  if (missing(xlim)) {
+    xlim <- c(min(z) - unit, max(z) + unit)
+  }
+  if (missing(ylim)) {
+    ylim <- c(0, 1.1 * ymax)
+  }
+  if (plotHist) {
+    h <- hist(z, breaks = seq(from = min(z) - unit, to = max(z) + unit, 
+                   by = unit), plot = FALSE)
+    ylim[2] <- max(ylim[2], 1 * 1 * h$count)
+    plot(h, col = "grey", main = main, xlab = deparse(substitute(z)), ylim = ylim, 
+         xlim = xlim)
+  } else {
     if (!add) {
-        arrows(min(z) - unit, ymax, min(z), ymax, angle = 90, code = 3, length = 0.1)
-        text(min(z), ymax, paste("unit =", unit), pos = 4)
+      plot(0, 0, type = "n", xlim = xlim, ylim = ylim, xlab = deparse(substitute(z)), 
+           ylab = "Frequency", main = main)
     }
+  }
+  
+  # here is the one line of shist-ty code:
+  lines(d$x, d$y * n * unit, ...)
+  
+  if (!add) {
+    arrows(min(z) - unit, ymax, min(z), ymax, angle = 90, code = 3, length = 0.1)
+    text(min(z), ymax, paste("unit =", unit), pos = 4)
+  }
 }
