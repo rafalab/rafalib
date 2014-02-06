2f14675968806152787d9bdc971b75ec3f1fc827
ririzarr <rairizarry@gmail.com>
2014-02-05 21:27:31 -0500

    edit

--- R/splot.R
+++ R/splot.R
@@ -1,10 +1,14 @@
 splot <- function(x,y,n=10000,subset=NULL,xlab=NULL,ylab=NULL,...){
     if(is.null(xlab)) xlab=deparse(substitute(x))
     if(is.null(ylab)) ylab=deparse(substitute(x))
-    if(is.null(subset)){
-        if(length(x)>n) subset=sample(length(x),10000)
+    if(!is.null(subset)){
+        x=x[subset]
+        y=y[subset]
     }
-    x=x[subset]
-    y=y[subset]
+    if(length(x)>n){
+        ind=sample(length(x),10000)
+        x=x[ind]
+        y=y[ind]
+    }
     plot(x,y,xlab=xlab,ylab=ylab,...)
 }
