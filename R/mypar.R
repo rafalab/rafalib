cfc1924b1090cf93ab8cc0207a6c8194ed7d8f7b
ririzarr <rairizarry@gmail.com>
2014-02-07 15:59:21 -0500

    omg! editing mypar

--- R/mypar.R
+++ R/mypar.R
@@ -1,6 +1,6 @@
 mypar <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",...){
     require(RColorBrewer)
-    par(mar=c(2.5,2.5,1.6,1.1),mgp=c(1.5,.5,0))
+    par(mar=c(2.75,2.5,1.6,1.1),mgp=c(1.5,.5,0))
     par(mfrow=c(a,b),...)
     palette(brewer.pal(brewer.n,brewer.name))
 }
