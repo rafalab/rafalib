62ce86af5efacaf40e6d3fd03be9702554f9a3dd
ririzarr <rairizarry@gmail.com>
2014-03-01 12:06:59 -0500

    changes to mypar

--- R/mypar.R
+++ R/mypar.R
@@ -7,7 +7,7 @@ mypar2 <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",mar=c(2.5,2.5,1.6,1.1)
 
 
 ##this is for R studio
-mypar <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",cex.lab=2,cex.main=2,cex.axis=1.5,mar=c(5.1,4.6,3.5,2.1),mgp=c(3,1,0),...){
+mypar <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",cex.lab=2,cex.main=2,cex.axis=1.5,mar=c(5.1,5.1,3.5,2.1),mgp=c(3,1,0),...){
     require(RColorBrewer)
     par(mar=mar,mgp=mgp,cex.lab=cex.lab,cex.main=cex.main,cex.axis=cex.axis)
     par(mfrow=c(a,b),...)
