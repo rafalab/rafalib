e9e57addb9ef1a8b3aaa6852d9b495ed957240b0
ririzarr <rairizarry@gmail.com>
2014-03-05 22:49:17 -0500

    change to getCelfileDates

--- R/getCelfileDates.R
+++ R/getCelfileDates.R
@@ -4,7 +4,7 @@ getCelfileDates <- function(filenames,...){
         tmp<-affyio::read.celfile.header(filenames[i],info="full")
 ##        dates[i]<-strsplit(tmp$ScanDate,"\ ")[[1]][1]
         ##some cell files do not have a space
-        dates[i]<-substring(tmp$ScanDate,1,9)
+        dates[i]<-substring(tmp$ScanDate,1,10)
     }
     dates<-as.Date(dates,"%m/%d/%y")
     return(dates)
