d834317de4704202016cae26a461ba683844e3f8
Mike Love <michaelisaiahlove@gmail.com>
2014-04-04 13:12:32 -0400

    messing up shist

--- R/shist.R
+++ R/shist.R
@@ -1,10 +1,12 @@
-shist <- function(z, unit = 1, plotHist = FALSE, add = FALSE,
-                  xlim, ylim, main, ...) {
+shist <- function(z, unit = .5 * sd(x),
+                  bw = unit, n, from, to,
+                  plotHist = FALSE, add = FALSE,
+                  xlab = deparse(substitute(z)),
+                  ylab = "Frequency",
+                  xlim, ylim,
+                  main = paste("Shistogram of", deparse(substitute(z))), ...) {
   n <- length(z)
-  if(missing(main)){
-      main <- paste("Shistogram of", deparse(substitute(z)))
-  }
-  d <- density(z)
+  d <- density(z, bw=bw, n=n, from=from, to=to)
   ymax <- max(d$y * n * unit)
   if (missing(xlim)) {
     xlim <- c(min(z) - unit, max(z) + unit)
@@ -16,12 +18,12 @@ shist <- function(z, unit = 1, plotHist = FALSE, add = FALSE,
     h <- hist(z, breaks = seq(from = min(z) - unit, to = max(z) + unit, 
                    by = unit), plot = FALSE)
     ylim[2] <- max(ylim[2], 1 * 1 * h$count)
-    plot(h, col = "grey", main = main, xlab = deparse(substitute(z)), ylim = ylim, 
-         xlim = xlim)
+    plot(h, col = "grey", main = main, xlab = xlab, ylab = ylab,
+         ylim = ylim, xlim = xlim)
   } else {
     if (!add) {
-      plot(0, 0, type = "n", xlim = xlim, ylim = ylim, xlab = deparse(substitute(z)), 
-           ylab = "Frequency", main = main)
+      plot(0, 0, type = "n", xlim = xlim, ylim = ylim, xlab = xlab,
+           ylab = ylab, main = main)
     }
   }
   
@@ -30,6 +32,6 @@ shist <- function(z, unit = 1, plotHist = FALSE, add = FALSE,
   
   if (!add) {
     arrows(max(z) - unit, ymax, max(z), ymax, angle = 90, code = 3, length = 0.1)
-    text(max(z)-unit, ymax, paste("unit =", unit), pos = 2)
+    text(max(z) - unit, ymax, paste("unit =", sprintf("%.3f", unit)), pos = 2)
   }
 }
