89e736b99e8e98e4c745272d7c05b22f519f44fd
Mike Love <michaelisaiahlove@gmail.com>
2014-05-06 10:16:00 -0400

    moving first cutoff to 20

--- R/sboxplot.R
+++ R/sboxplot.R
@@ -1,10 +1,11 @@
 sboxplot <- function(x, ...) {
+  cuts <- c(20, 100)
   lens <- sapply(x, length)
-  if (all(lens >= 100)) {
+  if (all(lens >= cuts[2])) {
     boxplot(x, range=0, ...)
-  } else if (all(lens >= 10 & lens < 100)) {
+  } else if (all(lens >= cuts[1] & lens < cuts[2])) {
     boxplot(x, ...)
-  } else if (all(lens < 10)) {
+  } else if (all(lens < cuts[1])) {
     stripchart(x, method="jitter", vertical=TRUE, pch=1, ...)
   } else {
     r <- range(unlist(x))
@@ -12,14 +13,14 @@ sboxplot <- function(x, ...) {
     n <- length(x)
     plot(0,0,type="n",xlab="",ylab="",xlim=c(0.5,n+.5),ylim=c(r[1] - .05 * d, r[2] + .05 * d),xaxt="n")
     for (i in seq_len(n)) {
-      if (lens[i] < 10) {
+      if (lens[i] < cuts[1]) {
         points(rep(i, lens[i]) + rnorm(lens[i], 0, .05), x[[i]])
-      } else if (lens[i] >= 10) {
+      } else if (lens[i] >= cuts[1]) {
         box <- quantile(x[[i]], c(.25, .75))
         med <- median(x[[i]])
         polygon(i + c(-.1,-.1,.1,.1), c(box, rev(box)))
         segments(i - .1, med, i + .1, med, lwd=3)
-        if (lens[i] < 100) {
+        if (lens[i] < cuts[2]) {
           iqr <- diff(box)
           out <- box[1] - x[[i]] > iqr | x[[i]] - box[2] > iqr
           segments(i, box[1], i, min(x[[i]][!out]))
