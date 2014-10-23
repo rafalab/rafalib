964548b3bc9fddda1c1d5b3da4ba668cb812c7ec
Mike Love <michaelisaiahlove@gmail.com>
2014-10-23 17:35:49 -0400

    adding two new ones

---
+++ R/imagemat.R
@@ -0,0 +1,23 @@
+#' image of a matrix
+#'
+#' produces an image of a matrix which matches
+#' the natural orientation
+#'
+#' @param x the matrix
+#' @param col the colors
+#' @param ... arguments passed to image
+#'
+#' @examples
+#'
+#' x <- matrix(c(1,0,0,0,1,
+#'               1,1,0,1,1,
+#'               1,0,1,0,1,
+#'               1,0,0,0,1,
+#'               1,0,0,0,1),
+#' ncol=5,byrow=TRUE)
+#'
+#' imagemat(x)
+#'
+imagemat <- function(x,col=colorRampPalette(c("white","black"))(9),...) {
+  image(t(x[nrow(x):1,]),col=col)
+}
