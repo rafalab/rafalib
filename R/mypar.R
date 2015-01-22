f1b3b18284c1c20e34b55a091935102b70e5afd1
ririzarr <rairizarry@gmail.com>
2015-01-22 14:42:57 -0500

    RI: mypar fix

--- R/mypar.R
+++ R/mypar.R
@@ -1,6 +1,6 @@
-mypar2 <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",mar=c(2.5,2.5,1.6,1.1),mgp=c(1.5,.5,0),...){
+mypar2 <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",cex.lab=1,cex.main=1.2,cex.axis=1,mar=c(2.5,2.5,1.6,1.1),mgp=c(1.5,.5,0),...){
     require(RColorBrewer)
-    par(mar=mar,mgp=mgp)
+    par(mar=mar,mgp=mgp,cex.lab=cex.lab,cex.main=cex.main,cex.axis=cex.axis)
     par(mfrow=c(a,b),...)
     palette(brewer.pal(brewer.n,brewer.name))
 }
@@ -31,3 +31,4 @@ mypar <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",cex.lab=2,cex.main=2,ce
     par(mfrow=c(a,b),...)
     palette(brewer.pal(brewer.n,brewer.name))
 }
+ 
