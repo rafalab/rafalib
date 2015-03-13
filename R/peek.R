9b9e647a3b4201c7589b38cd49741cf58049c8ab
Mike Love <michaelisaiahlove@gmail.com>
2015-03-13 10:24:16 -0400

    actually add peek

---
+++ R/peek.R
@@ -0,0 +1,8 @@
+#' peek at the top of a text file
+#'
+#' this returns a character vector which shows the top n lines of a file
+#'
+#' @param x a filename
+#' @param n the number of lines to return
+#' 
+peek <- function(x,n) scan(x,what="char",n=n,sep="\n")
