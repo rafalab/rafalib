0c391ecc01e26852832a4ccbb4e90ea4003b21e2
Mike Love <michaelisaiahlove@gmail.com>
2015-05-25 10:49:49 -0400

    new

---
+++ R/bartab.R
@@ -0,0 +1,17 @@
+#' bartab
+#'
+#' plot the overlap of three groups with a barplot
+#'
+#' @param x logical
+#' @param y logical
+#' @param z logical
+#' @param names a character vector of length 3
+#' 
+bartab <- function(x,y,z,names) {
+  tabs <- as.vector(table(x, y, z))
+  names(tabs) <- c("none",names[1],names[2],paste(names[1:2],collapse="+"),
+                   names[3],paste(names[c(1,3)],collapse="+"),
+                   paste(names[2:3],collapse="+"),"all")
+  tabs <- tabs[c(1,2,3,5,4,6,7,8)]
+  barplot(tabs)
+}
