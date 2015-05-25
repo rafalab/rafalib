cc5f7427be3db292b79a30063dafba8ec766ef84
Mike Love <michaelisaiahlove@gmail.com>
2015-05-25 10:58:48 -0400

    update

--- R/bartab.R
+++ R/bartab.R
@@ -6,12 +6,13 @@
 #' @param y logical
 #' @param z logical
 #' @param names a character vector of length 3
+#' @param ... to barplot
 #' 
-bartab <- function(x,y,z,names) {
+bartab <- function(x,y,z,names,...) {
   tabs <- as.vector(table(x, y, z))
   names(tabs) <- c("none",names[1],names[2],paste(names[1:2],collapse="+"),
                    names[3],paste(names[c(1,3)],collapse="+"),
                    paste(names[2:3],collapse="+"),"all")
   tabs <- tabs[c(1,2,3,5,4,6,7,8)]
-  barplot(tabs)
+  barplot(tabs, ...)
 }
