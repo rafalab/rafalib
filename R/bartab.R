bb9d695cf12e69a4e47ac41fbc09e6b9fea3ba04
Mike Love <michaelisaiahlove@gmail.com>
2015-05-25 11:11:14 -0400

    skip none

--- R/bartab.R
+++ R/bartab.R
@@ -6,9 +6,10 @@
 #' @param y logical
 #' @param z logical
 #' @param names a character vector of length 3
+#' @param skipNone remove the "none" group
 #' @param ... to barplot
 #' 
-bartab <- function(x,y,z,names,...) {
+bartab <- function(x,y,z,names,skipNone=FALSE,...) {
   x <- factor(x,c("FALSE","TRUE"))
   y <- factor(y,c("FALSE","TRUE"))
   z <- factor(z,c("FALSE","TRUE"))
@@ -17,5 +18,8 @@ bartab <- function(x,y,z,names,...) {
                    names[3],paste(names[c(1,3)],collapse="+"),
                    paste(names[2:3],collapse="+"),"all")
   tabs <- tabs[c(1,2,3,5,4,6,7,8)]
+  if (skipNone) {
+    tabs <- tabs[-1]
+  }
   barplot(tabs, ...)
 }
