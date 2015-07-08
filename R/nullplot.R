eb02a3a5ec8f71e01d94cf08a9204af2d8d1c54d
Mike Love <michaelisaiahlove@gmail.com>
2015-07-08 09:10:01 -0400

    tweak nullplot

--- R/nullplot.R
+++ R/nullplot.R
@@ -1,3 +1,3 @@
-nullplot <- function(x1,x2,y1,y2,xlab="",ylab="",...) {
+nullplot <- function(x1=0,x2=1,y1=0,y2=1,xlab="",ylab="",...) {
   plot(0,0,xlim=c(x1,x2),ylim=c(y1,y2),type="n",xlab=xlab,ylab=ylab,...)
 }
