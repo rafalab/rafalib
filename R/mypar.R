b6dab085ff53eebf12c560ff6d1c4d30e793894f
ririzarr <rairizarry@gmail.com>
2015-07-25 15:02:26 -0400

    final edits

--- R/mypar.R
+++ R/mypar.R
@@ -27,12 +27,12 @@
 mypar <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",cex.lab=1,cex.main=1.2,cex.axis=1,mar=c(2.5,2.5,1.6,1.1),mgp=c(1.5,.5,0),...){
     par(mar=mar,mgp=mgp,cex.lab=cex.lab,cex.main=cex.main,cex.axis=cex.axis)
     par(mfrow=c(a,b),...)
-    palette(brewer.pal(brewer.n,brewer.name))
+    palette(RColorBrewer::brewer.pal(brewer.n,brewer.name))
 }
 
 bigpar <- function(a=1,b=1,brewer.n=8,brewer.name="Dark2",cex.lab=2,cex.main=2,cex.axis=1.5,mar=c(5.1,5.1,3.5,2.1),mgp=c(3,1,0),...){
     par(mar=mar,mgp=mgp,cex.lab=cex.lab,cex.main=cex.main,cex.axis=cex.axis)
     par(mfrow=c(a,b),...)
-    palette(brewer.pal(brewer.n,brewer.name))
+    palette(RColorBrewer::brewer.pal(brewer.n,brewer.name))
 }
  
